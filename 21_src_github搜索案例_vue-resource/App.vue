d15507316430c0959dad03334fcc125505309aee
andrewbrook <andrewbrook@qq.com>
2023-11-09 13:57:26 +0800

    使用vue-resouce发生Ajax请求

---
+++ 21_src_github搜索案例_vue-resource/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container">
+		<Search/>
+		<List/>
+	</div>
+</template>
+
+<script>
+	// 这里引入样式会进行语法检查，不存在的资源会报错，在public中引入第三方css样式，可解决
+	// import './assets/css/bootstrap.css'
+	import Search from './components/Search'
+	import List from './components/List'
+
+	export default {
+		name:'App',
+		components:{Search,List}
+		
+	}
+</script>
+
+<style>
+	
+
+</style>
