657930eae4c5b6a3b0723bce57393dabb939f6ab
andrewbrook <andrewbrook@qq.com>
2023-11-11 19:25:19 +0800

    命名路由

---
+++ 33_src_命名路由/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Home组件内容</h2>
+        <div>
+            <ul class="nav nav-tabs">
+                <li>
+                <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+                <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+                </li>
+                <li>
+                <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+                <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+                </li>
+            </ul>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Home',
+        beforeDestroy(){
+            console.log('Home路由组件即将被销毁了')
+        },
+        mounted(){
+            console.log('Home路由组件挂载完毕了',this)
+        }
+    }
+</script>
+
+<style>
+
+</style>
