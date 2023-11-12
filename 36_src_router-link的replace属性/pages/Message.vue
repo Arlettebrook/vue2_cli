323c9768fff199f87a3d6e77d69f62b968c3d34b
andrewbrook <andrewbrook@qq.com>
2023-11-12 12:44:16 +0800

    router-link的replace属性

---
+++ 36_src_router-link的replace属性/pages/Message.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="m in messageList" :key="m.id">
+                <!-- <a href="/message1">{{ m.title }}</a>&nbsp;&nbsp; -->
+                <!-- to里面要是字符串，加：不能解析字符串 加``可以解析模板字符串 -->
+                <!-- 跳转路由并携带params参数：to的字符串写法 -->
+                <!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{ m.title }}</router-link>&nbsp;&nbsp; -->
+
+                <!-- 跳转路由并携带params参数：to的对象写法 -->
+                <router-link :to="{
+                    // 如果是params参数，不允许写path，只能用命名路由
+                    // path:'/home/message/detail',错，只能写name
+                    name:'xiangqing',
+                    // params:{
+                    query:{
+                        id:m.id,
+                        title:m.title,
+                    }
+                }">{{ m.title }}&nbsp;&nbsp; hi</router-link>
+            </li>
+        </ul>
+        <hr/>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Message',
+        data(){
+            return {
+                messageList:[
+                    {id:'001',title:'消息001'},
+                    {id:'002',title:'消息002'},
+                    {id:'003',title:'消息003'},
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
