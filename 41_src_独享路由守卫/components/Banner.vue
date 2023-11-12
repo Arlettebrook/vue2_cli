bb7af0b4ef8d1041324f593e7e1d93e90776ac4d
andrewbrook <andrewbrook@qq.com>
2023-11-12 17:57:38 +0800

    独享路由守卫

---
+++ 41_src_独享路由守卫/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue Router Demo</h2></div>
+        <button @click="back">后退</button>
+        <button @click="forward">前进</button>
+        <button @click="testGo">测试一下go</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Banner',
+    methods:{
+        back(){
+            console.log(this.$router)
+            this.$router.back()
+        },
+        forward(){
+            this.$router.forward()
+        },
+        testGo(){
+            // 正数是往前跳转指定次数，负数往后跳转指定次数
+            this.$router.go(2)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 41_src_独享路由守卫/pages/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2>Home组件内容</h2>
+        <div>
+            <ul class="nav nav-tabs">
+                <li>
+                <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+                <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+                </li>
+                <li>
+                <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+                <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+                </li>
+            </ul>
+            <!-- <router-view></router-view> -->
+            <!-- 缓存路由组件，切换不让它销毁 
+                属性include包括那个组件，没有默认全部
+                值是组件名，不是路由组件的name属性
+                要包括多个的时候用数组
+                include="['News','Messgae']"
+            -->
+            <keep-alive include="News">
+                <router-view/>
+            </keep-alive>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Home',
+        beforeDestroy(){
+            console.log('Home路由组件即将被销毁了')
+        },
+        mounted(){
+            console.log('Home路由组件挂载完毕了',this)
+        }
+    }
+</script>
+
+<style>
+
+</style>
