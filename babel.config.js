4124f7380a022fb6ce26c03ea227bca201fc70f0
andrewbrook <andrewbrook@qq.com>
2023-11-13 10:48:40 +0800

    element-ui组件库

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    // 缺什么依赖npm i ’‘ 安装就好，新版vue/cli：es2015改成@babel/preset-env
+    // ["es2015", { "modules": false }],
+    ["@babel/preset-env", { "modules": false }],
+  ],
+  // "plugins": [
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
